452dbf58e80107e9b66b3ca0362644023201680e
Maruf Ajagunna <marufajagunna@gmail.com>
2021-02-15 13:10:28 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { Component } from "react"; 
+import { CardList } from './components/cardList/CardList'; 
+import SearchBox from './components/searchBox/SearchBox';
 import './App.css';
+class App extends Component {
+  state = {
+    monsters: [],
+    searchField: ''
+  };
+  componentDidMount() { 
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((respnse) => respnse.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+  render() { 
+    const { monsters, searchField } = this.state;
+    const filteredMonster = monsters.filter( (monster) => 
+    monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()) );
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <div className='App'>
+      <h1> Monsters Rolodex </h1>
+      <SearchBox
+      placeholder = 'Search Monsters'
+      handleChange = {this.handleChange } 
+      />
+      <CardList monsters = {filteredMonster} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/searchBox/SearchBox.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./searchBox.css";
+const SearchBox = ({ placeholder, handleChange }) => {
+  return (
+    <div>
+      <input
+      className='search'
+        type="search"
+        placeholder= { placeholder }
+        onChange={ handleChange }
+      />
+    </div>
+  );
+};
+export default SearchBox;
